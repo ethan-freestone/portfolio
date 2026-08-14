eb99f761d72d9f1c27fe5309345fb312bf257237
ethan-freestone <ejfreebie@gmail.com>
2026-08-14 18:20:28 +0100

    feat: Wire up folio screenshots

---
+++ src/assets/projects/applications/folio-erm/index.ts
@@ -0,0 +1,8 @@
+export { default as pkg } from './package.png';
+export { default as packages } from './packages.png';
+export { default as agreementLineView } from './agreement-line-view.png';
+export { default as agreementSelector } from './agreement-selector.png';
+export { default as agreementView1 } from './agreement-view-1.png';
+export { default as agreementView2 } from './agreement-view-2.png';
+export { default as agreementsSettings } from './agreements-settings.png';
+export { default as licenseForm } from './license-form.png';
