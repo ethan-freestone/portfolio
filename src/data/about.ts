a8611be9e86e589ccbfc6d57688f2625683dbd58
Ethan Freestone <70844858+ethan-freestone@users.noreply.github.com>
2026-08-15 00:47:22 +0100

    chore: Update about.ts

--- src/data/about.ts
+++ src/data/about.ts
@@ -1,5 +1,6 @@
 import avatarImg from '@/assets/avatar.jpeg'
 import {
+  Cat,
   CodeXml,
   Gamepad2,
   Pickaxe,
@@ -43,8 +44,8 @@ export const PROFILE_DATA: ProfileData = {
   role: 'Full-Stack Software Engineer & Architect',
   avatarUrl: avatarImg,
   location: 'Sheffield, UK',
-  bio: "Building robust web applications in the library tech space since 2019. I specialize in untangling complex problem domains and designing scalable, elegant solutions that meet critical customer needs.",
-  bioDeep: "I work beest at the intersection of high-level system architecture and hands-on team leadership. Refactoring monolithic systems into extensible strategy patterns, building framework-agnostic libraries, and overhauling CI/CD infrastructure, I focus on creating abstractions that genuinely make sense. Beyond writing code, I am passionate about mentoring engineers, driving the adoption of modern developer tooling (DX), and fostering a collaborative environment to build accessible, user-first applications.",
+  bio: "Building web applications in the library tech space since 2019. I specialize in untangling complex problem spaces and designing scalable, elegant solutions that meet customer needs.",
+  bioDeep: "I work best when allowed to flow between tackling high-level system architecture and hands-on team leadership. Refactoring systems into extensible/maintainable patterns, building framework-agnostic libraries, and overhauling CI/CD infrastructure, my favourite work is in the abstraction behind the code that developers use day to day. Beyond writing code, I am passionate about mentoring engineers, working with modern developer tooling to improve the day to day work across a team, and fostering a collaborative environment to build accessible, user-first applications.",
   skills: [
     {
       category: 'Frontend',
@@ -105,13 +106,13 @@ export const PROFILE_DATA: ProfileData = {
       role: 'Senior Software Engineer & Team Leader',
       company: 'Knowledge Integration',
       period: '2024-2026',
-      description: 'Architecting and leading development across FOLIO, ILL, and DocDel platforms. Acting as a core technical strategist, I translate complex domain requirements into maintainable, high-level solutions. I actively mentor engineers, champion cross-team developer experience (DX) initiatives, and collaborate directly with stakeholders to ensure our architecture delivers long-term value.',
+      description: 'Architecting and leading development across FOLIO, and ILL platforms, as well as smaller projects that fit into the greater whole. Acting as a technical strategist, I work on turning complex domain requirements into maintainable, innovative solutions. I actively mentor engineers, champion cross-team developer experience initiatives, and collaborate directly with stakeholders to ensure our architecture delivers long-term value.',
     },
     {
       role: 'Software Engineer with DevOps Support',
       company: 'Knowledge Integration',
       period: '2019 — 2024',
-      description: 'Engineered complex applications for the FOLIO ecosystem (ERM suite, Open Access, Serials). Progressed from feature delivery to high-level system design, focusing heavily on software library development, testing architecture inversion, and standardizing shared UI components.',
+      description: 'Developed and engineered complex applications for the FOLIO ecosystem (ERM suite, Open Access, Serials). Progressed from feature delivery to high-level system design, focusing heavily on software library development, testing architecture inversion, and standardizing shared UI components. Work included interacting directly with customers to best deliver on their needs.',
     },
   ],
 
@@ -135,6 +136,11 @@ export const PROFILE_DATA: ProfileData = {
       Icon: Volleyball,
       name: 'Football',
       description: 'Playing weekly and following my beloved West Ham through the ups and downs (mostly downs).'
+    },
+    {
+      Icon: Cat,
+      name: 'Animals'
+      description: 'A total sucker for any and all creatures, fluffy, scaly or otherwise! Can often be found in an evening relaxing and watching a film cuddled with my two cats. (Kitty photos available on request!)'
     },
   ],
 };
